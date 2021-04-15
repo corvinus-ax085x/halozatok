72f9b7267af3788bc4dc547cd0a5600156b12938
Balázs Veréb <ipponbalazs@gmail.com>
2021-04-15 16:44:41 +0200

    kerdesek

---
+++ Hajosteszt/Hajosteszt/Controllers/EmptyBoat.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hajosteszt.Controllers
+{
+    public class EmptyBoat : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
